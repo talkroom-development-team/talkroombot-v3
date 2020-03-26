df165c0826cbade6b9943286303bb2e8c9dece2d
129.05 <minseo0388@outlook.com>
2020-03-26 17:15:50 +0900

    Código de programa impresionante

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
  * @description 수다방에 사용되는 수다방 공식 봇
  *
  * @license MIT
- * @author Initial T / comjun04(@comjun04) / minseo0388(@Naesung, 129.03) / PMH(@pmh-only) / Veloster(@velo64)
- *
+ * @author Initial T / comjun04(@comjun04, comjun04@gmail.com) / minseo0388(@Naesung, minseo0388@daum.net) / PMH(@pmh-only) / Ruby(@devruby, hiveruby@gmail.com) / Veloster(@velo64, moca1526@yandex.ru)
+ *  
  */
 
 const BotClient = require('./classes/BotClient.js')
