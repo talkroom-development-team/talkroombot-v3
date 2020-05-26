f2100f6b593007e7f96776094101f83627f393be
pmh-only <pmhstudio.pmh@gmail.com>
2020-05-26 20:11:20 +0900

    Oh, i forgot it

--- events/onMessage.js
+++ events/onMessage.js
@@ -5,6 +5,8 @@ const Query = require('../classes/Query')
  * @param {import('discord.js').Message} msg
  */
 function onMessage (client, msg, extensions) {
+  if (!msg.content.startsWith(client.setting.prefix)) return
+
   const query = new Query(client, msg)
   const find = client.commands.find((c) => c.aliases.includes(query.command))
   if (find) find.run(client, msg, extensions || {})
